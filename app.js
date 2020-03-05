e5d00ed9c582ab680ed354c05159e1b34e5b8075
akib17 <akibahmad17@gmail.com>
2020-03-05 15:35:55 +0600

    Get, load & show data on UI

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const obj = {
-    id: 12,
-    name: 'Akib Ahmad',
-    location: 'Chittagong',
-    email: 'akibahmad17@gmail.com'
-}
+// const obj = {
+//     id: 12,
+//     name: 'Akib Ahmad',
+//     location: 'Chittagong',
+//     email: 'akibahmad17@gmail.com'
+// }
 
-const objJSON = JSON.stringify(obj)
+// const objJSON = JSON.stringify(obj)
 
-const parseObj = JSON.parse(objJSON)
-console.log(objJSON);
-console.log(parseObj)
+// const parseObj = JSON.parse(objJSON)
+// console.log(objJSON);
+// console.log(parseObj)
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(data => data.json())
+//     .then(users => {
+//         jsonUsers(users)
+//     })
+
+// function jsonUsers(users) {
+//     const lists = document.getElementById('lists')
+//     users.map(el => {
+//         const li = document.createElement('li')
+//         li.className = 'list-group-item'
+//         li.innerHTML = el.name
+//         lists.appendChild(li)
+//     })
+// }
